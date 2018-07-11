924f8c635f725d1faa8940d54c48c85531c21d24
Ivan Rojo <ivanrojo07@gmail.com>
2018-07-11 12:26:20 -0500

    resolucion de ruta

--- app/Http/Controllers/Api/User/UserProductosFotosController.php
+++ app/Http/Controllers/Api/User/UserProductosFotosController.php
@@ -44,7 +44,9 @@ class UserProductosFotosController extends Controller
                 $image = str_replace('data:image/jpg;base64,', '', $image);
                 $image = str_replace(' ', '+', $image);
                 $imageName = str_random(10).'.'.'png';
-                Storage::put(storage_path(). '/' . $imageName, base64_decode($image));
+                // Storage::put(storage_path(). '/' . $imageName, base64_decode($image));
+                Storage::put($imageName, base64_decode($image));
+
                 $foto = FotoProducto::create([
                     'producto_id' => $producto->id,
                     'image_path' => '/'.$imageName,
